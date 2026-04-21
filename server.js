8307a0d49130758d21fa5282ae95b8b217bf76c2
siva7985 <153522319+siva7985@users.noreply.github.com>
2026-04-21 12:37:50 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,21 +4,27 @@ const app = express();
 app.use(express.json());
 
 let command = "";
+let latestData = {};
 
-// EA reads command
+// Command APIs
 app.get("/api/command", (req, res) => {
     res.send(command);
-    command = ""; // reset after sending
+    command = "";
 });
 
-// App / you send command
 app.post("/api/send-command", (req, res) => {
     command = req.body.command;
     res.send("OK");
 });
 
-app.get("/", (req, res) => {
-    res.send("Server Running");
+// NEW: Data APIs
+app.post("/api/update", (req, res) => {
+    latestData = req.body;
+    res.send("OK");
+});
+
+app.get("/api/data", (req, res) => {
+    res.json(latestData);
 });
 
 app.listen(3000, () => console.log("Server started"));
