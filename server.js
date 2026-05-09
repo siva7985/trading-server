12436c3c2fc610c09f2a64d7c0f472ca38705c1a
siva7985 <153522319+siva7985@users.noreply.github.com>
2026-05-09 13:20:06 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -23,11 +23,16 @@ const SECRET = "my_secret_key";
 /* =========================
    🔗 MONGODB CONNECT
 ========================= */
-mongoose.connect("mongodb+srv://admin:Nsrk798489@tradingapp.t6uqbxa.mongodb.net/trading_app?retryWrites=true&w=majority")
-  .then(async () => {
-    console.log("MongoDB Connected");
-  })
-  .catch(err => console.log(err));
+mongoose.connect(
+  "mongodb://admin:Nsrk798489@ac-xxxx-shard-00-00.x.mongodb.net:27017,ac-xxxx-shard-00-01.x.mongodb.net:27017,ac-xxxx-shard-00-02.x.mongodb.net:27017/trading_app?ssl=true&replicaSet=atlas-xxxxx-shard-0&authSource=admin&retryWrites=true&w=majority"
+)
+.then(() => {
+  console.log("MongoDB Connected ✅");
+})
+.catch(err => {
+  console.log("MongoDB ERROR ❌");
+  console.log(err);
+});
 
 /* =========================
    📦 USER MODEL
