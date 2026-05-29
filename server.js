df1ce388ad3e299704cf05b9a7230c91d779912e
siva7985 <153522319+siva7985@users.noreply.github.com>
2026-05-29 12:28:58 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -793,10 +793,10 @@ app.post("/api/update", verifySecret, async (req, res) => {
   // ✅ KEEP EXISTING SETTINGS
   const existingData = await Data.findOne({ account });
 
-  /*const finalSettings =
+  const finalSettings =
 	  settings && settings.length > 0
 		? settings
-		: existingData?.settings || [];*/
+		: existingData?.settings || [];
 
   await Data.findOneAndUpdate(
     { userId: user._id, account },
@@ -812,10 +812,7 @@ app.post("/api/update", verifySecret, async (req, res) => {
       prices,
       trades,
 
-      settings:
-		  existingData?.settings?.length > 0
-			? existingData.settings
-			: settings || [],
+      settings: finalSettings,
 
       eaRunning,
       mt5Connected,
