b5e3d87589b51990f862de1134b4b7b449fa6af8
siva7985 <153522319+siva7985@users.noreply.github.com>
2026-06-11 16:41:09 +0530

    Add files via upload

--- models/EAActivation.js
+++ models/EAActivation.js
@@ -1,1 +1,45 @@
+const mongoose = require("mongoose");
 
+const EAActivationSchema = new mongoose.Schema({
+
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+
+  account: {
+    type: String,
+    required: true
+  },
+
+  eaId: {
+    type: String,
+    required: true
+  },
+
+  status: {
+    type: String,
+    default: "active"
+  },
+
+  settings: {
+    lot: {
+      type: Number,
+      default: 0.01
+    },
+
+    risk: {
+      type: Number,
+      default: 1
+    }
+  }
+
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model(
+  "EAActivation",
+  EAActivationSchema
+);
